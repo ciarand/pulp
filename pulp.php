22c1d54433ad9f6a4b35e3c523379528774bf340
Ciaran Downey <me@ciarand.me>
2014-03-02 14:14:45 -0800

    Add some totally imaginary goal APIs to pulp.php
    Stuff like:
    
     - a static method for everything (::className()) that returns
       the fully namespaced class.
    
     - a concat plugin that returns one fd instead of a bag of them
    
     - a "requirePlugin" method on the Pulp object that creates a wrapping
       closure for the plugin

--- pulp.php
+++ pulp.php
@@ -1,16 +1,30 @@
-<?php
+<?php require "vendor/autoload.php";
 
-require "vendor/autoload.php";
+use Ciarand\Pulp\Pulp as Pulp;
+use Ciarand\Pulp\Plugins;
 
-use Ciarand\Pulp\Manager as Pulp;
+$pulp    = new Pulp;
+$concat  = $pulp->requirePlugin(Plugins\Concat::className());
+$assetic = $pulp->requirePlugin(Plugins\Assetic::className());
+$sass    = $assetic->createFilter("ScssphpFilter");
+$jsmin   = $assetic->createFilter("JSqueezeFilter");
 
-$pulp = new Pulp;
-
-$pulp->task("scss", [], function () use ($pulp) {
+$pulp->task("scss", [], function () use ($pulp, $sass) {
     $pulp->log("Compiling SCSS files");
 
     $pulp->src("**/*.scss")
+        ->pipe($sass(["enableCompass" => true]))
         ->pipe($pulp->dest("./dest/"));
+});
+
+$pulp->task("js", [], function () use ($pulp, $jsmin, $concat) {
+    $pulp->log("Minifying JS files");
+
+    $pulp->src("**/*.js")
+        ->pipe($concat())
+        ->pipe($pulp->dest("./dest/all.js"))
+        ->pipe($jsmin())
+        ->pipe($pulp->dest("./dest/all.min.js"));
 });
 
 $pulp->task("default", ["scss"], function () use ($pulp) {
